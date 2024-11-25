8ab36a7b497b9d95b45ab3308036ddd56fe9bde5
aditsvet <adi.tsvet.github@gmail.com>
2024-11-25 04:13:27 -0500

    title changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <html lang="en">
+      <head>
+      <title>Adnan Ali</title>
+      <link rel="icon" href="/logo.png" type="image/png" />
+      </head>
       <body className="bg-gray-50 dark:bg-gray-900 text-gray-800 dark:text-gray-200 transition-colors">
         <Navbar/>
         <main>{children}</main>
